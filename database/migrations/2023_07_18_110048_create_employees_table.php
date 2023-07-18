02da93418fcc1f2716cb9908af9ef78225ead7f4
HassanElshazlyEida <hassanaboeata@gmail.com>
2023-07-18 14:08:12 +0300

    💥 NEW: DB & Migrations & Seeders

--- database/migrations/2023_07_18_110048_create_employees_table.php
+++ database/migrations/2023_07_18_110048_create_employees_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,15 +12,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->foreignIdFor(Company::class)->constrained()
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+            $table->string('email');
             $table->string('phone');
-            $table->string('profile_picture')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('employees');
     }
 };
